314c3ccc17828d02a072188dac8567b94726d002
Joanne Lai <joanne.lai.2021@smu.edu.sg>
2026-08-11 15:06:04 +0800

    please

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import socket
 
 from routes import app
 from routes.api_gateway import api_gateway_bp
+from routes.toolbox1 import toolbox1_bp
 from routes.showdown import showdown_bp
 from routes.api_gateway_v2 import api_gateway_bp_v2
 
 logger = logging.getLogger(__name__)
 # app.register_blueprint(api_gateway_bp)
 app.register_blueprint(showdown_bp)
+app.register_blueprint(toolbox1_bp, url_prefix='/mcp')
 app.register_blueprint(api_gateway_bp_v2)
 
 @app.route('/', methods=['GET'])
