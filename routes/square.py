4b91c11872252bd7c901d47670d8ad5f5be39580
jiahui0309 <e0970466@u.nus.edu>
2026-08-11 17:00:04 +0800

    square

--- routes/square.py
+++ routes/square.py
@@ -1,18 +1,19 @@
-import json
 import logging
 
-from flask import request, Blueprint
-
-from routes import app
+from flask import request, Blueprint, jsonify
 
 logger = logging.getLogger(__name__)
 square_bp = Blueprint('square_bp', __name__)
 
 @square_bp.route('/square', methods=['POST'])
 def evaluate():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    input_value = data.get("input")
-    result = input_value * input_value
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    data = request.get_json(force=True, silent=True) or {}
+    logging.info("data sent for evaluation %s", data)
+
+    number = data.get("number")
+    if number is None or not isinstance(number, (int, float)):
+        return jsonify({"error": "Invalid input; expected a numeric 'number' field."}), 400
+
+    answer = number * number
+    logging.info("answer: %s", answer)
+    return jsonify({"answer": answer})
