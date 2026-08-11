b99ac14d190450e6f01422da2c05bcfda02a72ab
jiahui0309 <e0970466@u.nus.edu>
2026-08-11 14:59:39 +0800

    chore: remove v1

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from routes.showdown import showdown_bp
 from routes.api_gateway_v2 import api_gateway_bp_v2
 
 logger = logging.getLogger(__name__)
-app.register_blueprint(api_gateway_bp)
+# app.register_blueprint(api_gateway_bp)
 app.register_blueprint(showdown_bp)
 app.register_blueprint(api_gateway_bp_v2)
 
